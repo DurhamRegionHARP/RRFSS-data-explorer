7e7fbbf8ea6dc27d9c0cad951b20717e2f456586
James Lane <lanejames35@gmail.com>
2018-11-16 15:44:45 -0500

    uglify stylesheets with gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglifycss = require("gulp-uglifycss");
+
+gulp.task('join', function() {
+  return gulp.src(['./css/theme.css', './css/logos.css', './css/cards.css'])
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./css/'));
+});
+gulp.task('css', function () {
+  gulp.src('./css/styles.css')
+    .pipe(uglifycss({
+      "uglyComments": false
+    }))
+    .pipe(gulp.dest('./css/dist/'));
+});
+gulp.task('run', ['join', 'css']);
+gulp.task('default', ['run']);
