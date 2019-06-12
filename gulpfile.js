e3a34e4eebbf32d5640ac66fa253c2f407524792
James Lane <lanejames35@gmail.com>
2019-06-12 10:40:52 -0400

    minify svg images

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const concat = require('gulp-concat');
 const uglifycss = require('gulp-uglifycss');
 const uglifyjs = require('gulp-uglify');
 const rename = require('gulp-rename');
+const svgmin = require('gulp-svgmin');
 
 function css(){
   return src(['css/deps/*.css', './css/src/*.css'])
@@ -18,7 +19,13 @@ function js(){
          .pipe(uglifyjs())
          .pipe(rename({ extname: '.min.js' }))
          .pipe(dest('script/dist/'));
-
 }
 
+function svg(){
+  return src('assets/images/*.svg')
+        .pipe(svgmin())
+        .pipe(dest('assets/images/'));
+}
+
+exports.img = svg;
 exports.build = series(css, js);
